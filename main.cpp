172e584e6827714e174635a93cd5a828a0f07fa5
Alex-Rotella <arotella1221@gmail.com>
2022-02-20 18:31:24 -0500

    Cleaned Format

--- main.cpp
+++ main.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+
 using namespace std;
 
 char gameboard[3][3] = {'1','2','3','4','5','6','7','8','9'};
@@ -33,9 +34,9 @@ void userMove() {
     do {
         cout << "Enter the number where you would like to place your selection" << endl;
         cin >> choice;
-        for(int i = 0; i < 3; i++){
-            for(int j = 0; j < 3; j++){
-                if(gameboard[i][j] == choice){
+        for(int i = 0; i < 3; i++) {
+            for(int j = 0; j < 3; j++) {
+                if(gameboard[i][j] == choice) {
                     gameboard[i][j] = changePlayer();
                     cout << "Found" << endl;
                     valid = true;
@@ -45,8 +46,7 @@ void userMove() {
                 
             }     
         }
-        if(valid == false)
-        {
+        if(valid == false) {
             cout << "Spot has already been chosen, pick again" << endl;
         }
     } while(valid == false);
@@ -61,19 +61,16 @@ char winner(){
         return 'X';
     if (gameboard[2][0] == 'X' && gameboard[2][1] == 'X' && gameboard[2][2] == 'X')
         return 'X';
- 
     if (gameboard[0][0] == 'X' && gameboard[1][0] == 'X' && gameboard[2][0] == 'X')
         return 'X';
     if (gameboard[0][1] == 'X' && gameboard[1][1] == 'X' && gameboard[2][1] == 'X')
         return 'X';
     if (gameboard[0][2] == 'X' && gameboard[1][2] == 'X' && gameboard[2][2] == 'X')
         return 'X';
- 
     if (gameboard[0][0] == 'X' && gameboard[1][1] == 'X' && gameboard[2][2] == 'X')
         return 'X';
     if (gameboard[2][0] == 'X' && gameboard[1][1] == 'X' && gameboard[0][2] == 'X')
         return 'X';
- 
     // O wins
     if (gameboard[0][0] == 'O' && gameboard[0][1] == 'O' && gameboard[0][2] == 'O')
         return 'O';
@@ -81,28 +78,24 @@ char winner(){
         return 'O';
     if (gameboard[2][0] == 'O' && gameboard[2][1] == 'O' && gameboard[2][2] == 'O')
         return 'O';
- 
     if (gameboard[0][0] == 'O' && gameboard[1][0] == 'O' && gameboard[2][0] == 'O')
         return 'O';
     if (gameboard[0][1] == 'O' && gameboard[1][1] == 'O' && gameboard[2][1] == 'O')
         return 'O';
     if (gameboard[0][2] == 'O' && gameboard[1][2] == 'O' && gameboard[2][2] == 'O')
         return 'O';
- 
     if (gameboard[0][0] == 'O' && gameboard[1][1] == 'O' && gameboard[2][2] == 'O')
         return 'O';
     if (gameboard[2][0] == 'O' && gameboard[1][1] == 'O' && gameboard[0][2] == 'O')
         return 'O';
  
-    return '/';
+    return '1';
 }
 
-void newGame()
-{
+void newGame() {
     int counter = 1;
-    for(int i = 0; i < 3; i++){
-            for(int j = 0; j < 3; j++)
-            {
+    for(int i = 0; i < 3; i++) {
+            for(int j = 0; j < 3; j++) {
                 gameboard[i][j] = '0' + counter;
                 counter++;
             }     
@@ -114,35 +107,28 @@ void newGame()
 int replay() {
     cout << "Would you like to play again? (Y/N)" << endl;
     cin >> playAgain;
-    if(playAgain == 'Y' || playAgain == 'y')
-    {
+    if(playAgain == 'Y' || playAgain == 'y') {
         newGame();
         drawboard();
         return 1;
     }
-    else
-    {
-        exit;
+    else {
         return 0;
     }
 }
 
-int main()
-{
+int main() {
     drawboard();
-    while (1)
-    {
+    while (1) {
         userMove();
         drawboard();
-        if (winner() == 'X')
-        {
+        if (winner() == 'X') {
             cout << "X wins!" << endl;
             if(replay() == 0){
                 return 0;
             }
         }
-        else if (winner() == 'O')
-        {
+        else if (winner() == 'O') {
             cout << "O wins!" << endl;
             if(replay() == 0){
                 return 0;
